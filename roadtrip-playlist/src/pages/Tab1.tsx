7445141daa0570af83a41004430b324fec6d1f51
dmj130 <78328374+dmj130@users.noreply.github.com>
2021-03-15 12:18:16 -0500

    Update Tab1.tsx

--- roadtrip-playlist/src/pages/Tab1.tsx
+++ roadtrip-playlist/src/pages/Tab1.tsx
@@ -1,36 +1,60 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonGrid, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge, IonActionSheet, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonGrid, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonReactRouter } from '@ionic/react-router'; 
+import { calendar, person, call , settings, personCircle, map, informationCircle, trash, share, caretForwardCircle, heart, close } from 'ionicons/icons';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
-const Tab1: React.FC = () => {
+const Roadtrip_Playlists: React.FC = () => {
+
+
+
   return (
     <IonPage>
+
+	
+
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+        <IonToolbar  color="primary">
+	   
+          <IonTitle >Roadtrip Playlists</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Roadtrip Playlists</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonCard>
+        <IonCard color="medium">
           <IonCardHeader>
-            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-            <IonCardTitle>Card Title</IonCardTitle>
+            
+            <IonCardTitle>Give us a destination and let us handle the rest!
+	    </IonCardTitle>
           </IonCardHeader>
 
-          <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile,
-            and climb a mountain or spend a week in the woods. Wash your spirit clean.
-      </IonCardContent>
+         
         </IonCard>
+
+	<IonCard>
+	<IonCardContent>
+
+<IonButton expand="full" size="large" color="success" href="/tab3"> Connect to Spotify</IonButton>
+
+<IonButton expand="full" size="large" color="secondary" href="/tab2"> Choose your destination</IonButton>
+
+<IonButton expand="full" size="large" color="warning" href="/tab4">create your personal playlist</IonButton>
+
+
+	</IonCardContent>
+	</IonCard>
+
+
+
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab1;
+export default Roadtrip_Playlists;
