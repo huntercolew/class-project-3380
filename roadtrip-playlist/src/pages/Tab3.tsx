8453bf6a1e15f8c973b8714e95f5ce356e31a3c6
dmj130 <78328374+dmj130@users.noreply.github.com>
2021-03-15 12:19:03 -0500

    Update Tab3.tsx

--- roadtrip-playlist/src/pages/Tab3.tsx
+++ roadtrip-playlist/src/pages/Tab3.tsx
@@ -1,13 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonItem, IonInput, IonButton, IonCard, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+
+ const [text, setText] = useState<string>();
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle>Spotify</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +18,21 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+	<IonCard>
+       <IonCardHeader color="medium">
+	 <IonCardTitle>Choose your destination to map out your musical adventure.
+	    </IonCardTitle>
+	</IonCardHeader>
+       </IonCard>
+<IonCard color="secondary">
+	
+	<IonItem>
+            <IonInput  value={text} placeholder="Enter Destination" onIonChange={e => setText(e.detail.value!)}></IonInput>
+	</IonItem>
+
+	</IonCard>
+        
       </IonContent>
     </IonPage>
   );
